8e2b911506537a3c277b21f0c76f0c87a40dd429
mina <mina693@hanmail.net>
2017-09-21 11:11:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/b100/seongbook/SuccessActivity.java
@@ -0,0 +1,17 @@
+package com.example.b100.seongbook;
+
+/**
+ * Created by B100 on 2017-09-15.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+}
